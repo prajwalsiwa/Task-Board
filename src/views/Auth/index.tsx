aeca3a1516413b347219303478a287a4aac05cea
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-22 12:52:39 +0545

    feat: create auth component

---
+++ src/views/Auth/index.tsx
@@ -0,0 +1,17 @@
+import LoginForm from '@Components/common/Login/LoginForm';
+import kanbanImage from '@Assets/images/kanban.png';
+
+function Auth() {
+  return (
+    <div className="grid h-full w-full lg:grid-cols-2 ">
+      <div className="flex h-full w-full items-center justify-center bg-blue-700 lg:w-full ">
+        <img src={kanbanImage} alt="" className="h-full w-full object-cover" />
+      </div>
+      <div className="flex h-full w-full items-center justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
