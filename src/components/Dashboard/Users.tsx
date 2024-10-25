535fd051a907b8dab74ca9e51d969cdf266ed24f
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-25 14:23:25 +0545

    feat: create users component

---
+++ src/components/Dashboard/Users.tsx
@@ -0,0 +1,7 @@
+import RoundedContainer from '@Components/common/RoundedContainer';
+
+function Users() {
+  return <RoundedContainer className="w-full">hello</RoundedContainer>;
+}
+
+export default Users;
