c7406d19a9c4b0c40e1f08bafcac048df2e3d10e
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-25 14:09:00 +0545

    feat: create prioritychart component

---
+++ src/components/Dashboard/PriorityChart.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable no-nested-ternary */
+import ChartHeader from '@Components/common/Charts/ChartHeader';
+import CustomDonutChart from '@Components/common/Charts/DonutChart';
+import DonutLegendItem from '@Components/common/Charts/DonutLegendItem';
+import { useRef } from 'react';
+
+const priorityList = [
+  {
+    name: 'High',
+    value: 7,
+  },
+  {
+    name: 'Medium',
+    value: 2,
+  },
+  {
+    name: 'Low',
+    value: 1,
+  },
+];
+
+function PriorityChart() {
+  const downloadComponentRef = useRef();
+
+  return (
+    <div className="relative  h-full w-full rounded-xl  border bg-grey-50 px-4 py-2">
+      <ChartHeader
+        chartTitle="Tasks By Priority"
+        downloadComponentRef={downloadComponentRef}
+      />
+      <div className="flex h-full items-center justify-center gap-2">
+        <CustomDonutChart
+          data={priorityList}
+          fills={['#4b5563', '#6b7280', '#9ca3af']}
+        />
+        <div className="flex h-full w-[25rem] flex-col justify-center ">
+          {priorityList.map((priority, index) => (
+            <DonutLegendItem
+              key={priority.name}
+              color={
+                index === 0 ? '#475569' : index === 1 ? '#6b7280' : '#9ca3af'
+              }
+              name={priority.name}
+              value={priority.value}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PriorityChart;
