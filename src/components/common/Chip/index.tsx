3d224c13698a2e45e26146e3a8879b04b5c6491c
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-26 10:45:15 +0545

    feat: add conditional icon in chip

--- src/components/common/Chip/index.tsx
+++ src/components/common/Chip/index.tsx
@@ -5,20 +5,29 @@ import capitalizeFirstLetter from '@Utils/capitalizeFirstLetter';
 interface IChipProps {
   label: string | ReactElement;
   onClose: any;
+  showIcon?: boolean;
+  className?: string;
 }
 
-export default function Chip({ label, onClose }: IChipProps) {
+export default function Chip({
+  label,
+  onClose,
+  showIcon = false,
+  className,
+}: IChipProps) {
   return (
     <div
-      className="flex h-8 cursor-pointer items-center gap-1
-      rounded-lg border border-grey-300 bg-grey-100 px-2 text-sm"
+      className={`flex h-8 cursor-pointer items-center gap-1
+      rounded-lg border border-grey-300 bg-grey-100 px-2 text-sm ${className}`}
     >
       <p>{capitalizeFirstLetter(label.toString())}</p>
-      <Icon
-        onClick={onClose}
-        name="close"
-        className="!text-icon-sm font-bold "
-      />
+      {showIcon && (
+        <Icon
+          onClick={onClose}
+          name="close"
+          className="!text-icon-sm font-bold "
+        />
+      )}
     </div>
   );
 }
