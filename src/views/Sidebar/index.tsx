5ccfedfde0aa91d173ae7a246bac790f6183558d
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-28 22:19:21 +0545

    fix: edit ui

--- src/views/Sidebar/index.tsx
+++ src/views/Sidebar/index.tsx
@@ -35,7 +35,7 @@ const sidebarList = [
 
 function SideBar() {
   return (
-    <div className="flex h-full w-72 flex-col gap-4 bg-slate-100 shadow-2xl">
+    <div className="flex h-full w-72 flex-col gap-4 bg-slate-100 pt-4 shadow-2xl">
       <div className="flex h-20 w-full items-center gap-2  px-3">
         <div className="flex w-full items-center justify-center gap-2 rounded-lg border py-2 shadow-md">
           <div className="flex h-8 w-8 items-center justify-center rounded-lg border  bg-gray-500 text-xl font-semibold text-white">
@@ -47,7 +47,7 @@ function SideBar() {
       <div className="w-full rounded-lg border  px-8 py-4 text-sm font-semibold text-gray-500">
         MY PAGES
       </div>
-      <div className="flex h-full w-full  flex-col gap-4 px-8 py-2 ">
+      <div className="flex h-full w-full  flex-col gap-3 px-8 py-2 ">
         {sidebarList.map(item => (
           <SidebarItem
             key={item.id}
