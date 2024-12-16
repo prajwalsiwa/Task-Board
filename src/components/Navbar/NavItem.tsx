a9a9a2f00b41f27dcb864028ef0109a50fd19167
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-12-16 10:06:05 +0545

    feat: add authenctication login

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,12 +1,18 @@
 // import SearchInput from '@Components/common/FormUI/SearchInput';
 import Icon from '@Components/common/Icon';
+import { Button } from '@Components/RadixComponents/Button';
+import { useTypedSelector } from '@Store/hooks';
+import { useNavigate } from 'react-router-dom';
 
 function NavItem() {
+  const { name } = useTypedSelector(state => state.sidebar.activeState);
+  const navigate = useNavigate();
+  const isAuthenticated = localStorage.getItem('token');
   return (
-    <div className="flex   h-10 w-full">
+    <div className="flex   h-14 w-full">
       <div className="flex w-full justify-between">
         <div className="flex h-full items-center   ">
-          <h4 className="text-white">Main Dashboard</h4>
+          <h4 className="text-white">{name}</h4>
         </div>
         {/* <div className="h-full ">
           <SearchInput
@@ -19,9 +25,18 @@ function NavItem() {
         <div className="flex">
           <Icon name="notifications" className="text-white" />
         </div>
-        <div className="flex h-10 w-10 items-center justify-center rounded-full border bg-orange-600 text-lg text-white">
-          PS
-        </div>
+        {isAuthenticated ? (
+          <div className="flex h-10 w-10 items-center justify-center rounded-full border bg-orange-600 text-lg text-white">
+            E
+          </div>
+        ) : (
+          <Button
+            onClick={() => navigate('/login')}
+            className="bg-grey-300 text-lg text-grey-800 hover:border hover:border-grey-800 hover:bg-grey-500 hover:text-white "
+          >
+            Sign In
+          </Button>
+        )}
       </div>
     </div>
   );
